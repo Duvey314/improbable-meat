cd0512aa6da1de6a6a63f5285af2bca91370cb44
Duvey <rudow.david@gmail.com>
2020-08-18 14:39:24 -0500

    removed practice files

--- app.js
+++
@@ -1,9 +0,0 @@
-d3.json("samples.json").then(function(data){
-    console.log(data);
-});
-
-d3.json("samples.json").then(function(data){
-    firstPerson = data.metadata[0];
-    Object.entries(firstPerson).forEach(([key, value]) =>
-      {console.log(key + ': ' + value);});
-});
